92da5c5695d298b21bf4707ed79f8813eaca712f
Sksham Kaushal <skshamkaushal@gmail.com>
2025-07-07 11:36:03 +0530

    done

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
-import express from "express"
-import cors from "cors"
-import 'dotenv/config'
-import cookieParser from "cookie-parser"
-import connectDB from "./config/mongodb.js"
-import authRouter from './routes/authRoutes.js'
-import userRouter from './routes/userRoutes.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT||4000;
+const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173','https://no-1-mern-auth.netlify.app/']
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://no-1-mern-auth.netlify.app",
+];
 
 app.use(express.json());
-app.use(cookieParser());  
-app.use(cors({origin:allowedOrigins ,credentials: true}))
-
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 //Api endpoints
-app.get('/',(req,res)=>res.send("API WORKING"));
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
+app.get("/", (req, res) => res.send("API WORKING"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(port,()=> console.log(`server is running on http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`server is running on http://localhost:${port}`)
+);
